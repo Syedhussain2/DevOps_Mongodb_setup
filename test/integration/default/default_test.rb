674d1adea3dce16e4e52f4d9ebca58e5ec05d21d
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-10-26 12:27:03 +0100

    mongodb working

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,20 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+
+describe package 'mongodb-org' do
+  it { should be_installed }
+  its ('version') { should match /3\./ }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe service "mongod" do
+  it {should be_running}
+  it {should be_enabled}
+end
+# test that checks what process and address is running on this port
+
+describe port(27017) do
+  it { should be_listening}
+  its('processes') {should include 'mongod'}
+  its('addresses') {should include '0.0.0.0'} # needs calrifications
 end
